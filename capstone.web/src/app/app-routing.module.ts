1bc18c8a015dc35a8f903a6c71e8d14238defdd0
Ruchita Vani <56015636+RuchitaVani@users.noreply.github.com>
2024-09-24 17:58:47 -0600

    app routing to the main landing page

--- capstone.web/src/app/app-routing.module.ts
+++ capstone.web/src/app/app-routing.module.ts
@@ -6,18 +6,20 @@ import { HeaderComponent } from './header/header.component';
 import { AuthGuard } from './core/security/auth.guard';
 import { PriorityListComponent } from './priority-list/priority-list.component';
 import { PriorityComponent } from './priority/priority.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: '', component: HeaderComponent, canActivate: [AuthGuard] }, // Example default page
+  { path: '', component: NavigationComponent, canActivate: [AuthGuard] }, // Example default page
   { path: 'prioritys', component: PriorityListComponent },
   { path: 'priority', component: PriorityComponent },
-  { path: "priority/:id", component: PriorityComponent },
-  { path: '**', redirectTo: '' }
+  { path: 'priority/:id', component: PriorityComponent },
+  // { path: 'navigation', component: NavigationComponent },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
