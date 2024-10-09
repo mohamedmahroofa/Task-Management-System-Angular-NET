6be005e32a94b69a5715c1a0564faa275a5b7f12
mohamedmahroofa <mohamedmahroofa@mymacewan.ca>
2024-10-08 20:42:16 -0600

    Updated prirority-list.html with material design

--- capstone.web/src/app/priority-list/priority-list.component.ts
+++ capstone.web/src/app/priority-list/priority-list.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class PriorityListComponent implements OnInit{
   prioritys: Priority[] = []; // Array to hold list of persons
-  displayedColumns: string[] = ['name', 'createdDate'];
+  displayedColumns: string[] = ['name', 'dateCreated'];
   
  
     constructor(
