bbd5653713ec208ef67384fe54edd756d55c6b84
Anjali Patel <patela88@mymacewan.ca>
2024-10-15 17:58:11 -0600

    Connect Login CSS with html file, now Login CSS is working.

--- capstone.web/src/app/login/login.component.ts
+++ capstone.web/src/app/login/login.component.ts
@@ -4,8 +4,10 @@ import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
 })
+
 export class LoginComponent {
   username: string = '';
   password: string = '';
