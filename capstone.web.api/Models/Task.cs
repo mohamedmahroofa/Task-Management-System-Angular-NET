39146b176ee5d5306fb5e11ad9ffc61ef4820147
alapiscodavilaj <alapiscodavilaj@mymacewan.ca>
2024-10-22 17:38:49 -0600

    Adding a model for task entity, starting to build backend. #746

---
+++ capstone.web.api/Models/Task.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace capstone.web.api.Models
+{
+    public class Task
+    {
+        public int TaskId { get; set; }
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+        public bool IsDeleted { get; set; }
+
+        //Forgein Key
+        public int CategoryId { get; set; }
+        public int PriorityId { get; set; }
+
+
+        //Navigational properties
+        [JsonIgnore]
+        public Category? Category { get; set; }
+        public Priority? Priority { get; set; }
+
+    }
+}
