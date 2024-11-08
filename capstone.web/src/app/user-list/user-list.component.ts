6ea6c2190133358c1094f7cf9cc0dacf646ef369
mohamedmahroofa <mohamedmahroofa@mymacewan.ca>
2024-11-07 18:26:57 -0700

    imported User, Userservice and Router for User-list component

--- capstone.web/src/app/user-list/user-list.component.ts
+++ capstone.web/src/app/user-list/user-list.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user';
+import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-list',
