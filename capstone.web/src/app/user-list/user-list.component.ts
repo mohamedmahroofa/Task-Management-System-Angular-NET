edf78cd43c51f1f77aefd518d43a714e17001b46
mohamedmahroofa <mohamedmahroofa@mymacewan.ca>
2024-11-17 22:46:00 -0700

    Updated the user-list to display firstname, lastname,username,email and role in user-list.component.ts

--- capstone.web/src/app/user-list/user-list.component.ts
+++ capstone.web/src/app/user-list/user-list.component.ts
@@ -8,6 +8,32 @@ import { Router } from '@angular/router';
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css'
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit{
+  users: User[] = []; // Array to hold list of users
+  displayedColumns: string[] = ['firstName', 'lastName', 'username', 'email', 'role'];
+
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) {}
+  
+  ngOnInit(){
+    // Fetch Prioritys on component initialization
+    this.userService.getUsers().subscribe((data) => {
+     this.users = data;
+   });
+}
+
+  // Navigate to User details component
+  viewUser(id: number) {
+    this.router.navigate(['/user', id]);
+  }
+  
+  addUser() {
+    this.router.navigate(['/user']);
+  }
+  goBack() {
+    this.router.navigate(['/']);
+  }
 
 }
