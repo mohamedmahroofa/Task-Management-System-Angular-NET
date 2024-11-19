4c97d5d3c8b8349a14f3bb7e046885a8dc3577fb
mohamedmahroofa <mohamedmahroofa@mymacewan.ca>
2024-11-18 23:18:22 -0700

    User can see the list of full Users

--- capstone.web/src/app/user-list/user-list.component.ts
+++ capstone.web/src/app/user-list/user-list.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class UserListComponent implements OnInit{
   users: User[] = []; // Array to hold list of users
-  displayedColumns: string[] = ['firstName', 'lastName', 'username', 'email', 'role'];
+  displayedColumns: string[] = ['firstName', 'lastName', 'username', 'email', 'role', 'action'];
 
   constructor(
     private userService: UserService,
