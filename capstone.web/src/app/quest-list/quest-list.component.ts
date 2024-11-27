8a918c47678364dfb80a4b241a9baf0fc85dc843
alapiscodavilaj <alapiscodavilaj@mymacewan.ca>
2024-11-27 08:59:42 -0700

    Logic for fltering added to quest-list.component.ts #751

--- capstone.web/src/app/quest-list/quest-list.component.ts
+++ capstone.web/src/app/quest-list/quest-list.component.ts
@@ -6,6 +6,7 @@ import { Category } from '../models/category';
 import { Priority } from '../models/priority';
 import { CategoryService } from '../services/category.service';
 import { PriorityService } from '../services/priority.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
     selector: 'app-quest-list',
@@ -13,13 +14,22 @@ import { PriorityService } from '../services/priority.service';
     styleUrl: './quest-list.component.css',
     standalone: false
 })
+
 export class QuestListComponent implements OnInit{
   quests: Quest[] = []; // Array to hold list of quests
+  filteredQuests: Quest[] = []; // Array to hold list of  filtered quest by priority or category
   priorities: Priority[] = []; // Array to hold list of priorities
   categories: Category[] = []; // Array to hold list of categories
   displayedColumns: string[] = ['name', 'dueDate' , 'category', 'priority', 'action','actiondelete'];
-  filteredQuests = this.quests;
- 
+  
+  //Using FormControl for Priority Filter
+  priorityFilter = new FormControl('');
+
+  //Using FormControl for Category Filter
+  categoryFilter = new FormControl('');
+  
+  
+
     constructor(
       private questService: QuestService,
       private priorityService: PriorityService,
@@ -31,6 +41,7 @@ export class QuestListComponent implements OnInit{
        // Fetch Quests on component initialization
        this.questService.getQuests().subscribe((data) => {
         this.quests = data;
+        this.filteredQuests =[...this.quests];
       });
 
       this.priorityService.getPrioritys().subscribe((data) => {
@@ -40,6 +51,21 @@ export class QuestListComponent implements OnInit{
       this.categoryService.getCategories().subscribe((data) => {
         this.categories = data;
       });
+
+      this.priorityFilter.valueChanges.subscribe((value) => {
+        this.applyFilter();
+      });
+  }
+
+  applyFilter() {
+    const selectedPriority = this.priorityFilter.value;
+
+    if (!selectedPriority) {
+      this.filteredQuests = [...this.quests];
+    } else {
+      const selectedPriorityNumber = Number(selectedPriority);
+      this.filteredQuests = this.quests.filter((quest) => quest.priorityId === selectedPriorityNumber);
+    }
   }
 
   
@@ -52,6 +78,7 @@ export class QuestListComponent implements OnInit{
     if (confirm('Are you sure you want to delete this quest?')) {
       this.questService.deleteQuest(id).subscribe(() => {
         this.quests = this.quests.filter(quest => quest.questId !== id);
+        this.applyFilter();
       });
     }
   }
