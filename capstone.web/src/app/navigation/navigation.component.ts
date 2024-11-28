7d10fa3152a5a1a9a317fcf7f1d2ddc810cf181f
mohamedmahroofa <mohamedmahroofa@mymacewan.ca>
2024-11-27 23:05:49 -0700

    Update the navigation component and html to use logout function properly

--- capstone.web/src/app/navigation/navigation.component.ts
+++ capstone.web/src/app/navigation/navigation.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-navigation',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrl: './navigation.component.css'
 })
 export class NavigationComponent {
+  constructor(private userService: UserService, private router: Router) { }
 
+  isAuthenticated(): boolean {
+    return this.userService.isAuthenticated();
+  }
+
+  logout(): void {
+    if (confirm('Do you want to logout?')) {
+      this.userService.logout();
+      this.router.navigate(['/login']);
+    }
+  }
 }
