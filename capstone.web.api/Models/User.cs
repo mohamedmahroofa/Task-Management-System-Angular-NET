87889e47961e567260706c5eeff1a944d03ebe9e
mohamedmahroofa <ahm.sheedh@gmail.com>
2026-02-18 16:30:23 -0500

    Changed the User ID from int to string.

--- capstone.web.api/Models/User.cs
+++ capstone.web.api/Models/User.cs
@@ -4,7 +4,7 @@ namespace capstone.web.api.Models
 {
     public class User
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
