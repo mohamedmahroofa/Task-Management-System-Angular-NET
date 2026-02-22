026a9315af2a43a24b55eea5b62078dbadb8ee27
mohamedmahroofa <ahm.sheedh@gmail.com>
2026-02-22 00:54:38 -0500

    Edited User.cs to inslude mongoDB.Bson

--- capstone.web.api/Models/User.cs
+++ capstone.web.api/Models/User.cs
@@ -1,9 +1,13 @@
 ﻿using System.Text.Json.Serialization;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace capstone.web.api.Models
 {
     public class User
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
