2c573977361c14eb83e02a27a6960913ba815e84
mohamedmahroofa <ahm.sheedh@gmail.com>
2026-03-01 23:45:58 -0500

    Added Priority model to match mongoDB

--- capstone.web.api/Models/Priority.cs
+++ capstone.web.api/Models/Priority.cs
@@ -1,9 +1,13 @@
 ﻿using System.Text.Json.Serialization;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace capstone.web.api.Models
 {
     public class Priority
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
         public string PriorityId { get; set; }
         public string Name { get; set; }
         public DateTime DateCreated { get; set; }
