74bcf054812753afbe8d0139e278e7e20bfda9a4
mohamedmahroofa <ahm.sheedh@gmail.com>
2026-03-06 01:17:29 -0500

    Updated Quest Model for MongoDB.

--- capstone.web.api/Models/Quest.cs
+++ capstone.web.api/Models/Quest.cs
@@ -1,9 +1,13 @@
 ﻿using System.Text.Json.Serialization;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace capstone.web.api.Models
 {
     public class Quest
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
         public string QuestId { get; set; }
         public string Name { get; set; }
         public DateTime DateCreated { get; set; }
